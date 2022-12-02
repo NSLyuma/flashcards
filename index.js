17bc007f1a8f731bcd0266fd5a4e68a3d6248464
EduardA <akhnazarov1990@gmail.com>
2022-12-02 11:43:07 +0300

    Added reading files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const readlineSync = require('readline-sync');
 const fs = require('fs');
 
+function getQuestion(string) {
+  const content = fs.readFileSync(`./topics/${string}`,'utf-8');
+  return content.split('\n')
+}
+
 function quiz() {
   console.log('Hello! Enter your name');
   const userName = readlineSync.question('--> ');
@@ -8,8 +13,18 @@ function quiz() {
   console.log(`\n${userName}, выбери тему \n`);
   const folder = fs.readdirSync('./topics');
   console.log(folder.map((file) => file.slice(0, -4)).join('\n'));
+  let themeChoice = readlineSync.question('--> ');
+  console.log(themeChoice)
+  for (let i=0; i<folder.length; i+=1) {
+    if(folder[i].slice(0,1) === themeChoice) {
+      const currentQuestion = folder[i]
+      console.log(getQuestion(currentQuestion))
+    }
+  }
 }
+
 
 quiz();
 
 // сделать разноцветным
+// сделать проверку что пользователь при выборе темы ввел число
