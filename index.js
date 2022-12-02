e8aad3e2fd6de86c0dd2d6a89f0a500ab47a9462
Mariia <pravdova97@mail.ru>
2022-12-02 11:39:07 +0300

    for pull

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 const fs = require('fs');
 const chalk = require('chalk');
-
-const fsread = fs.readFileSync(
-  './topics/nighthawk_flashcard_data.txt',
-  'utf-8'
-);
-
-const arr = fsread.split('\n');
-console.log(chalk.blue(arr));
